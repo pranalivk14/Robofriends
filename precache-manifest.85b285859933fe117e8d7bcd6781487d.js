4e30daf2475d90116e6174adc8a45e1298cefa91
Pranali Kulkarni <pranalivk14@gmail.com>
2022-10-04 00:51:25 -0700

    Updates

---
+++ precache-manifest.85b285859933fe117e8d7bcd6781487d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2d60755ac457237d4e8",
+    "url": "/robofriends/static/css/main.b0a601b3.chunk.css"
+  },
+  {
+    "revision": "d2d60755ac457237d4e8",
+    "url": "/robofriends/static/js/main.d2d60755.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "b77bffe312c950a8f897",
+    "url": "/robofriends/static/css/2.ad6b78bd.chunk.css"
+  },
+  {
+    "revision": "b77bffe312c950a8f897",
+    "url": "/robofriends/static/js/2.b77bffe3.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "3eb2be46a75f69a75ac6142a350c062c",
+    "url": "/robofriends/static/media/Patchy Robots.3eb2be46.woff"
+  },
+  {
+    "revision": "19dd1ca3917e044386cfef2045f936ef",
+    "url": "/robofriends/index.html"
+  }
+];
